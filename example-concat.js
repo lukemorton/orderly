edd9643aa99b9070dfa45ca0995331573da71dc5
Luke Morton <lukemorton.designs@gmail.com>
2011-06-01 19:26:59 +0100

    Update whitespace to 4 spaces.

--- example-concat.js
+++ example-concat.js
@@ -1,23 +1,23 @@
 var orderly = new Orderly();
 
 orderly.queue(['example/js/*.js', 'example/css/*.css'])
-	.process(function (item, success) {
-		findAllFiles(item, function (files) {
-			success(files);
-		});
-	})
-	.process(function (files, success) {
-		var fileType = getFileType(files);
-		concatFiles(files, function (concatStr) {
-			success([fileType, concatStr]);
-		});
-	})
-	.process(function (concat, success) {
-		var newFilename = 'concat.' + concat[0];
-		strToFile(newFilename, concat[1], function () {
-			success(newFilename);
-		});
-	})
-	.complete(function (files) {
-		console.log(files.join("\n"));
-	});
+    .process(function (item, success) {
+        findAllFiles(item, function (files) {
+            success(files);
+        });
+    })
+    .process(function (files, success) {
+        var fileType = getFileType(files);
+        concatFiles(files, function (concatStr) {
+            success([fileType, concatStr]);
+        });
+    })
+    .process(function (concat, success) {
+        var newFilename = 'concat.' + concat[0];
+        strToFile(newFilename, concat[1], function () {
+            success(newFilename);
+        });
+    })
+    .complete(function (files) {
+        console.log(files.join("\n"));
+    });
